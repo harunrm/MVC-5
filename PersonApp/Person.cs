c26ad4f70ff6bdc1701d367d84f7134da356ca5c
harunrm <harungeo@yahoo.com>
2014-09-10 10:06:20 +0600

    Person app

---
+++ PersonApp/Person.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PersonApp
+{
+    class Person
+    {
+        public string firstName = "";
+        public string middleName = "";
+        public string lastName = "";
+
+        public string GetFullName()
+        {
+            return firstName + " " + middleName + " " + lastName;
+        }
+
+        public string GetReverseName()
+        {
+            char[] a = GetFullName().ToCharArray();
+            string reverseName = "";
+            
+            for(int i = a.Length - 1; i >=0; i--)
+            {
+                reverseName += a[i];
+            }
+            return reverseName;
+        }
+    }
+}
---
+++ PersonApp/PersonUI.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace PersonApp
+{
+    public partial class PersonUI : Form
+    {
+        Person p = new Person();
+
+        public PersonUI()
+        {
+            InitializeComponent();
+        }
+
+        private void getFullNameButton_Click(object sender, EventArgs e)
+        {
+            p.firstName = firstNameTextBox.Text;
+            p.middleName = middleNameTextBox.Text;
+            p.lastName = lastNameTextBox.Text;
+
+            MessageBox.Show(p.GetFullName());
+        }
+
+        private void getReverseNameButton_Click(object sender, EventArgs e)
+        {
+            p.firstName = firstNameTextBox.Text;
+            p.middleName = middleNameTextBox.Text;
+            p.lastName = lastNameTextBox.Text;
+
+            MessageBox.Show(p.GetReverseName());
+        }
+    }
+}
